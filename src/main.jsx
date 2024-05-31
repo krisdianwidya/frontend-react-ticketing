3e860c3527b9817f478da0b98e62003d71695024
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-05-31 12:19:57 +0700

    add primereact config - related to styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <PrimeReactProvider>
     <App />
-  </React.StrictMode>,
+  </PrimeReactProvider>
+  // </React.StrictMode>,
 )
