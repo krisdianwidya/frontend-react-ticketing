6876dafbc6b0f338a248c40e9762e7fac704d4da
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-05-31 21:01:54 +0700

    adjust top menu title naming logic - to set title value according to current path

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,12 +1,24 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Avatar } from 'primereact/avatar';
 
 const TopMenu = () => {
+    const [pageName, setPageName] = useState("")
+
+    const location = useLocation()
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            setPageName("Overview")
+        } else if (location.pathname === '/tickets') {
+            setPageName("Tickets")
+        }
+    }, [location.pathname]);
 
     return (
         <div className="flex justify-content-between mt-4 text-700">
-            <span className="font-semibold text-2xl">Overview</span>
+            <span className="font-semibold text-2xl">{pageName}</span>
 
             <div className="flex align-items-center">
                 <i className="pi pi-search mr-5"></i>
