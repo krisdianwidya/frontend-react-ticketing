a63c9f7cfc65724b470e389d39cee0f3c9ca2f68
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-06-01 23:23:56 +0700

    initialize create ticket dialog form

--- src/pages/TicketListing.jsx
+++ src/pages/TicketListing.jsx
@@ -1,13 +1,17 @@
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Tag } from 'primereact/tag';
+import { Toolbar } from "primereact/toolbar";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { RadioButton } from "primereact/radiobutton";
+import { Dialog } from "primereact/dialog";
 import { classNames } from 'primereact/utils';
-
-
 
 export default function TicketListing() {
     const [lazyState, setlazyState] = useState({
@@ -18,11 +22,10 @@ export default function TicketListing() {
         sortOrder: null
     });
 
-    const useFetchProducts = () => {
+    const useFetchTickets = () => {
         return useQuery({
             queryKey: ["tickets"],
             queryFn: async () => {
-                // console.log('log fetch', lazyState);
                 const { data, headers } = await axios.get(`${import.meta.env.VITE_BASE_URL}support-tickets?page=${lazyState.page + 1}&limit=${lazyState.rows}&sort=${lazyState.sortField}&order=${lazyState.sortOrder === 1 ? 'asc' : 'desc'}`);
                 return {
                     tickets: data,
@@ -38,9 +41,61 @@ export default function TicketListing() {
         });
     };
 
-    const { isLoading, data, error, refetch } = useFetchProducts();
-    if (isLoading) return <h1>Loading</h1>;
+    const { isLoading, data, error, refetch } = useFetchTickets();
     if (error) return <h1>Error</h1>;
+
+    const leftToolbarTemplate = () => {
+        return (
+            <div className="flex flex-wrap gap-2">
+                <Button label="Create" icon="pi pi-plus" severity="success" onClick={openNew} />
+            </div>
+        );
+    };
+
+    const [ticketDialog, setTicketDialog] = useState(false)
+
+    const openNew = () => {
+        setTicket(emptyTicket);
+        // setSubmitted(false);
+        setTicketDialog(true);
+    };
+
+    const hideDialog = () => {
+        // setSubmitted(false);
+        setTicketDialog(false);
+    };
+
+    const saveTicket = () => {
+        console.log('save');
+    }
+
+    const ticketDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" icon="pi pi-times" outlined onClick={hideDialog} />
+            <Button label="Save" icon="pi pi-check" onClick={saveTicket} />
+        </React.Fragment>
+    );
+
+    let emptyTicket = {
+        id: null,
+        title: '',
+        description: '',
+        status: 'open',
+        priority: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+        customer: {
+            name: ''
+        },
+        due_at: new Date(),
+        assigned_to: {
+            id: null,
+            name: "",
+            email: ""
+        }
+    };
+
+    const [ticket, setTicket] = useState(emptyTicket);
 
     const header = (<h4 className="m-0">All Tickets</h4>);
 
@@ -116,6 +171,8 @@ export default function TicketListing() {
 
     return (
         <div className="card mt-5">
+            <Toolbar className="mb-4" left={leftToolbarTemplate}></Toolbar>
+
             <DataTable
                 value={data.tickets}
                 lazy
@@ -138,6 +195,40 @@ export default function TicketListing() {
                 <Column field="priority" header="Priority" sortable style={{ minWidth: '12rem' }} body={priorityBodyTemplate} />
                 <Column field="status" header="Status" sortable style={{ minWidth: '12rem' }} body={statusBodyTemplate} />
             </DataTable>
+
+            <Dialog visible={ticketDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Create a new ticket" modal className="p-fluid" footer={ticketDialogFooter} onHide={hideDialog}>
+                <div className="field">
+                    <label htmlFor="title" className="font-bold">
+                        Title
+                    </label>
+                    <InputText id="title" value={ticket.title} required autoFocus />
+
+                </div>
+                <div className="field">
+                    <label htmlFor="description" className="font-bold">
+                        Description
+                    </label>
+                    <InputTextarea id="description" value={ticket.description} required rows={3} cols={20} />
+                </div>
+
+                <div className="field">
+                    <label className="mb-3 font-bold">Priority</label>
+                    <div className="formgrid grid">
+                        <div className="field-radiobutton col-4">
+                            <RadioButton inputId="priority1" name="priority" value="Low" checked={ticket.priority === 'Low'} />
+                            <label htmlFor="priority1">Low</label>
+                        </div>
+                        <div className="field-radiobutton col-4">
+                            <RadioButton inputId="priority2" name="priority" value="Medium" checked={ticket.priority === 'Medium'} />
+                            <label htmlFor="priority2">Medium</label>
+                        </div>
+                        <div className="field-radiobutton col-4">
+                            <RadioButton inputId="priority3" name="priority" value="High" checked={ticket.priority === 'High'} />
+                            <label htmlFor="priority3">High</label>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
         </div>
     );
 }
